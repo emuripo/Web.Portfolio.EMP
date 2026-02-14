ce9b5c39b803bee850d1ec5252f01022b9087093
emuripo <esteban.murilloporras@gmail.com>
2026-02-13 19:27:21 -0600

    test: add playwright smoke tests

---
+++ tests/i18n.spec.ts
@@ -0,0 +1,9 @@
+import { expect, test } from "@playwright/test";
+
+test("language toggle switches EN to ES", async ({ page }) => {
+  await page.goto("/");
+  const toggle = page.getByRole("button", { name: /EN|ES/ });
+  await toggle.click();
+
+  await expect(page.getByRole("link", { name: /Impacto|Sobre mí|Inicio/ })).toBeVisible();
+});
